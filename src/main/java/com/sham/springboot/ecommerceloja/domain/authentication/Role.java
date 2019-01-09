786cb015048b4aadd7b4283c3b60b06c80b30012
skatesham <sham.vinicius@gmail.com>
2019-01-09 16:06:48 -0200

    fixing authentication

--- src/main/java/com/sham/springboot/ecommerceloja/domain/authentication/Role.java
+++ src/main/java/com/sham/springboot/ecommerceloja/domain/authentication/Role.java
@@ -1,6 +1,5 @@
 package com.sham.springboot.ecommerceloja.domain.authentication;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
@@ -21,7 +20,6 @@ public class Role implements GrantedAuthority {
 	private static final long serialVersionUID = -7198514176046036731L;
 
 	@Id
-	@Column(unique = true, nullable = false)
 	private String name;
 	
 	public Role(RoleEnum role) {
